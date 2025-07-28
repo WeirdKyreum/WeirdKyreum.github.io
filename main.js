ced31eb81844a495a048234f169478e62981e9f4
WeirdKyreum <gavobernal@gmail.com>
2025-07-28 15:35:25 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,81 +1,57 @@
-// const aField = document.querySelector("#aField");
-// const bField = document.querySelector("#bField");
-// const sumBox = document.querySelector("#sum-box");
-// const sumButton = document.querySelector("#sum");
-// sumButton.addEventListener("click", doSum);
-// function doSum() {
-// //.value is property to get data from input element
-// //parseInt to convert into number
-// let a = parseInt(aField.value);
-// let b = parseInt(bField.value);
-// let sum = a+b;
-// sumBox.innerHTML = "Sum of "+a+" and "+b+" is " + sum +".";
-// }
 
-
-// var min=0, max=100, currGuess=-100, counter=0;
-// var ans=Math.round(Math.random()*(max-min)+min);
-// console.log("Ans:"+ans);
-// const btnGuess=document.querySelector("#btnGuess");
-// const guessField=document.querySelector("#guessField");
-// const smallerlbl=document.querySelector("#smaller");
-// const biggerlbl=document.querySelector("#bigger");
-// const commentsBox=document.querySelector("#commentsBox");
-// btnGuess.addEventListener("click",GuessFn);
-
-// function GuessFn(){
-// currGuess=parseInt(guessField.value);
-// console.log("Curr Guess:"+currGuess);
-// let comments="";
-// if(currGuess==ans){
-// comments="CORRECT!";
-// }
-// if(currGuess>ans){
-// //replace the right number with current guess
-// biggerlbl.innerHTML=currGuess;
-// comments="too Big!";
-// }
-// if(currGuess<ans){
-// //replace the left number with current guess
-// smallerlbl.innerHTML=currGuess;
-// comments="too Small!";
-// }
-// guessField.value="";
-// counter++;
-// //commentsBox.innerHTML="Your guess: "+currGuess+" is "+comments+”;
-// Tries:"+counter";
-// //use template literals (backticks)
-// commentsBox.innerHTML=`Your guess: ${currGuess} is ${comments} Tries:
-// ${counter}`;
-// }
-
-const btnSubmit = document.querySelector("#btnSubmit");
-const scorebox = document.querySelector("#scorebox");
-
-var q1, q2, score = 0;
-
-function CheckAns() {
-    // read the value of the selected radio button for q1
-    q1 = document.querySelector("input[name='q1']:checked").value;
-    console.log(q1); // check q1 value retrieved
-
-    // read the value of the selected radio button for q2
-    q2 = document.querySelector("input[name='q2']:checked").value;
-    console.log(q2); // check q2 value retrieved
-
-    // reset score to 0, check answers, and give score if correct
-    score = 0;
-    if (q1 == "Tokyo") score++;
-    if (q2 == "Red") score++;
-
-    scorebox.innerHTML = "Score: " + score;
+function GetRandom(min, max) {
+    //this will select a number between min and max
+    return Math.round(Math.random() * (max - min)) + min;
+}
+function MoveDurian() {
+    durianId.style.left = GetRandom(0, 500) + "px";
+    durianId.style.top = GetRandom(0, 500) + "px";
+    durianId.classList.add("anim1");
+    durianId.classList.remove("shrink");
 }
 
-arrayAns=["Tokyo","Red"];
-for (let i = 1; i < 3; i++ ){
-    CheckAns()
+var moveDurianItvId = setInterval(MoveDurian, 2000);
+const stopBtn=document.querySelector("#stopBtn");
+stopBtn.addEventListener("click",function(){
+    clearTimeout(moveDurianItvId);
+    stopBtn.classList.toggle("newState");
+    stopBtn.classList.add("anim1");
+})
+
+const scoreBox = document.getElementById("scoreBox");
+
+const popAudio = new Audio("");
+//create an new Audio Object using sound file
+
+var score = 0;    //to track how many clicks
+function durianCatch() {
+    //increases score after clicking
+    score++;
+    //update html scorebox
+    scoreBox.innerHTML = "Score: " + score;
+    popAudio.play(); //play the audio!
+
+    //make durian shrink and fade!
+    durianId.classList.add("shrink");
+    //undo durian rotate animation
+    durianId.classList.remove("anim1");
 }
+//link durian to mouseclick to durianCatch function
+durianId.addEventListener("click", durianCatch);
 
-
-
-btnSubmit.addEventListener("click", CheckAns);
+//keyevents to test durian transition and anmiation works
+document.addEventListener("keydown", function (evt) {
+    console.log(evt);
+    if (evt.code == "KeyT") {
+        durianId.classList.add("shrink");
+    }
+    if (evt.code == "KeyU") {
+        durianId.classList.remove("shrink");
+    }
+    if (evt.code == "KeyA") {
+        durianId.classList.add("anim1");
+    }
+    if (evt.code == "KeyB") {
+        durianId.classList.remove("anim1");
+    }
+});
